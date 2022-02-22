7125b77d0034941d913be370f0442b8aa3518d49
gcy <gcy@zhihu.com>
2022-02-22 11:35:47 +0800

    add AggregatorV3Interface

--- simple_storage/FoudMe.sol
+++ simple_storage/FoudMe.sol
@@ -2,6 +2,53 @@
 
 pragma solidity >=0.6.6 <0.9.0;
 
+// 代码来自： https://github.com/smartcontractkit/chainlink/blob/develop/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol
+interface AggregatorV3Interface {
+    function decimals() external view returns (uint8);
+
+    function description() external view returns (string memory);
+
+    function version() external view returns (uint256);
+
+    // getRoundData and latestRoundData should both raise "No data present"
+    // if they do not have data to report, instead of returning unset values
+    // which could be misinterpreted as actual reported values.
+    function getRoundData(uint80 _roundId)
+        external
+        view
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        );
+
+    function latestRoundData()
+        external
+        view
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        );
+}
+
 contract FoudMe {
-    function fund() public payable {}
+    // 谁向合约付了多少钱
+    mapping(address => uint256) public addressToAmountFunded;
+
+    function fund() public payable {
+        addressToAmountFunded[msg.sender] += msg.value;
+    }
+
+    function getVersion() public view returns (uint256) {
+        // address https://docs.chain.link/docs/ethereum-addresses/#Rinkeby%20Testnet
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        );
+        return priceFeed.version();
+    }
 }
